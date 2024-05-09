e151515ea1392721d34f5b1d2701b4b3ecbd9bb6
Thomas Desse <dessethomas@gmail.com>
2024-05-09 14:49:45 +0200

    clean code

--- js/script-index.js
+++ js/script-index.js
@@ -7,19 +7,8 @@ const btnMoreInfos = document.querySelector(".js-btn");
 const moreInfo = document.querySelector(".content__more-infos");
 
 
-// openMenu.addEventListener("click", function() {
-//     navPage.classList.remove("nav__page-close");
-//     navPage.classList.add("nav__page-open");
-
-// })
 Tools.openMenu(openMenu, navPage);
 Tools.closeMenu(closeMenu, navPage);
-
-// closeMenu.addEventListener("click", function() {
-//     navPage.classList.remove("nav__page-open");
-//     navPage.classList.add("nav__page-close");
-
-// })
 
 btnMoreInfos.addEventListener("click", function(){
     moreInfo.classList.toggle("hidden")
